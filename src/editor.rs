99034c89dcf3d952c6af1538f4c765bce2e78b32
qianyan <qianyan@thoughtworks.com>
2024-01-02 12:17:29 +0800

    feat: full of Tilde(~)

--- src/editor.rs
+++ src/editor.rs
@@ -28,6 +28,9 @@ impl Editor {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
         if self.should_quit {
             println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            print!("{}", termion::cursor::Goto(1, 1));
         }
         io::stdout().flush()
     }
@@ -39,6 +42,12 @@ impl Editor {
             _ => (),
         }
         Ok(())
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
+        }
     }
 }
 
