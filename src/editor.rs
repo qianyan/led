859f6b3060a0b8a938bd462b012efa7be6e0d4c4
qianyan <qianyan@thoughtworks.com>
2024-01-02 12:24:28 +0800

    feat: get the screen size to draw

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,11 @@
 use std::io::{self, stdout, Write};
 use termion::{event::Key, input::TermRead, raw::IntoRawMode};
 
+use crate::Terminal;
+
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
@@ -21,7 +24,10 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
@@ -45,7 +51,7 @@ impl Editor {
     }
 
     fn draw_rows(&self) {
-        for _ in 0..24 {
+        for _ in 0..self.terminal.size().height {
             println!("~\r");
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod editor;
+mod terminal;
 use editor::Editor;
+pub use terminal::Terminal;
 
 fn main() {
     Editor::default().run();
