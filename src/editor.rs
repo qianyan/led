8fed7e5ea393adc80066cb1486b14fdd1f2d5f0f
qianyan <qianyan@thoughtworks.com>
2024-01-03 09:39:41 +0800

    refactor: use derive macro in Position

--- src/editor.rs
+++ src/editor.rs
@@ -2,6 +2,7 @@ use crate::Document;
 use crate::Terminal;
 use termion::event::Key;
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -34,13 +35,13 @@ impl Editor {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             document: Document::default(),
-            cursor_position: Position { x: 0, y: 0 },
+            cursor_position: Position::default(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
