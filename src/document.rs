8cf3d22b50ac544f07fabd24974bdf29fb65c428
qianyan <qianyan@thoughtworks.com>
2024-01-12 14:58:39 +0800

    feat: Ctrl-S to save

--- src/document.rs
+++ src/document.rs
@@ -1,4 +1,7 @@
-use std::{fs, io::Error};
+use std::{
+    fs,
+    io::{Error, Write},
+};
 
 use crate::{Position, Row};
 
@@ -75,5 +78,16 @@ impl Document {
             let row = self.rows.get_mut(at.y).unwrap();
             row.delete(at.x);
         }
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        if let Some(file_name) = &self.file_name {
+            let mut file = fs::File::create(file_name)?;
+            for row in &self.rows {
+                file.write_all(row.as_bytes())?;
+                file.write_all(b"\n")?;
+            }
+        }
+        Ok(())
     }
 }
